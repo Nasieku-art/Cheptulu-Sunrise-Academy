37b50dc6d52748a6c3255c985671cf72d5ff7428
Nasieku-art <nasiekunasiekumary77@gmail.com>
2026-08-05 10:59:15 +0300

    changed the layout structure and amde few changes in page.js

--- cheptulu/app/page.js
+++ cheptulu/app/page.js
@@ -1,17 +1,15 @@
 "use client";
 import Link from "next/link";
-import Navbar from "./navbar/page";
+
 import Homepage from "./home/page";
-import Footer from "./footer/page";
+
 import AdminLogin from "./adminlogin/page";
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <Homepage />
       <AdminLogin />
-      <Footer />
     </div>
   );
 }
