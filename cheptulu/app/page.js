19d7665e8145400ba33aa1c1719873a6b54bd999
Nasieku-art <nasiekunasiekumary77@gmail.com>
2026-08-05 11:00:08 +0300

    removed the admin page from homepage

--- cheptulu/app/page.js
+++ cheptulu/app/page.js
@@ -3,13 +3,11 @@ import Link from "next/link";
 
 import Homepage from "./home/page";
 
-import AdminLogin from "./adminlogin/page";
 
 export default function Home() {
   return (
     <div>
       <Homepage />
-      <AdminLogin />
     </div>
   );
 }
